1bcf587f071dac1c88c3d9a7533bf9a53aa42d21
ashwinder24 <ashwinderbhinder24@gmail.com>
2017-08-03 13:12:16 -0400

    weekly project

--- main.js
+++ main.js
@@ -1,114 +1,107 @@
-// The Form Data
-let formData = [
-  // For demonstration purposes,
-  // the first form element has been added to the HTML file as a comment
-  // compare the input in the HTML file with the contents of this first object
-  {
-    "type": "text",
-    "label": "First Name",
-    "id": "user-first-name",
-    "icon": "fa-user",
-    "options": []
-  },
-  {
-    "type": "text",
-    "label": "Last Name",
-    "id": "user-last-name",
-    "icon": "fa-user",
-    "options": []
-  },
-  {
-    "type": "email",
-    "label": "Email Address",
-    "id": "user-email",
-    "icon": "fa-envelope",
-    "options": []
-  },
-  {
-    "type": "text",
-    "label": "Current Website URL",
-    "id": "user-website",
-    "icon": "fa-globe",
-    "options": []
-  },
-  {
-    "type": "select",
-    "label": "Select Language",
-    "id": "user-language",
-    "icon": "",
-    "options": [
-      {
-        "label": "English",
-        "value": "EN"
-      },
-      {
-        "label": "French",
-        "value": "FR"
-      },
-      {
-        "label": "Spanish",
-        "value": "SP"
-      },
-      {
-        "label": "Chinese",
-        "value": "CH"
-      },
-      {
-        "label": "Japanese",
-        "value": "JP"
-      }
-    ]
-  },
-  {
-    "type": "textarea",
-    "label": "Your Comment",
-    "id": "user-comment",
-    "icon": "fa-comments",
-    "options": []
-  },
-  {
-    "type": "tel",
-    "label": "Mobile Number",
-    "id": "user-mobile",
-    "icon": "fa-mobile-phone",
-    "options": []
-  },
-  {
-    "type": "tel",
-    "label": "Home Number",
-    "id": "user-phone",
-    "icon": "fa-phone",
-    "options": []
+// Since the `data.js` file is loaded in your `index.html` before this one,
+// you have access to the `data` variable that contains the Etsy Store data.
+// Open your `index.html` page in your browser and you can verify that the following
+// is properly working. The `data` variable is an array with 25 items in it
+console.log(data);
+
+
+
+// 1: Show me how to calculate the average price of all items.
+function question1 () {
+let total = 0;
+for (let i = 0; i < data.length; i++){
+
+  sum = total += data[i].price / data.length;
   }
-];
 
-// HINTS:
-// As you can see, we access the first element in the array
-// with [0] and then grab the property "label" using the "." operator
-( function(){
-  // Select the first element from the array
-  let first = formData[ 0 ];
-  // Log the first object
-  console.log( first );
-  // Log the string "First Name"
-  console.log( first.label );
-} )();
+  console.log(sum);
+
+}
+//  Return aver;
 
 
-// -------- Your Code Goes Below this Line --------
-let field = document.querySelector(".fields");
 
-for (var i = 0; i < formData.length; i++) {
- formData[i]
 
-let formArea = document.createElement("form");
-let formInput = document.createElement("input");
-field.appendChild( formArea );
-formArea.appendChild( formInput );
-formInput.setAttribute("src", formData[ i ].type);
-formInput.setAttribute("src", formData[ i ].label);
-formInput.setAttribute("src", formData[ i ].id);
-formInput.setAttribute("src", formData[ i ].options);
-formInput.setAttribute("src", formData[ i ].icon);
 
+// 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
+function question2 () {
+
+  for (let i = 0; i < data.length; i++){
+    cost = data[i].price;
+      items = data[i].title;
+    if (cost >= 14 && cost <=18){
+    // cost >= 14 && cost <=18);
+    console.log(items);
+  }
+}  //  if
+//      return data.title[]
+  //  }
+  // }
+
+  // Answer:
+}
+
+
+
+// 3: Which item has a "GBP" currency code? Display it's name and price.
+function question3 () {
+  for (let i = 0; i < data.length; i++){
+    cost = data[i].price;
+    items = data[i].title;
+    currencyType = data[i].currency_code;
+    if (currencyType === "GBP"){
+
+    console.log(items,cost);}
+  }// Answer:
+}
+
+
+// 4: Display a list of all items who are made of wood.
+function question4 () {
+  // Answer:for (let i = 0; i < data.length; i++){
+  for (let i = 0; i < data.length; i++){
+    items = data[i].title;
+    for (let j = 0; j < data[i].materials.length; j++) {  
+      materialType = data[i].materials[j];
+    
+    //type = materialType[j];
+    if (materialType === "wood"){
+
+    console.log(items + " is made of wood.");}
+    }
+  }
+}
+
+
+// 5: Which items are made of eight or more materials?
+//    Display the name, number of items and the items it is made of.
+function question5 () {
+    for (let i = 0; i < data.length; i++){
+         items = data[i].title;
+         itemsNumber = data[i].state;
+        
+    for (let j = 0; j < data[i].materials.length; j++) {  
+        items = data[i].title;
+      materialType = data[i].materials[j].length;
+    
+    //type = materialType[j];
+    if (materialType >= 8){
+
+    console.log(items);}
+    }
+  }
+  // Answer:
+}
+
+
+// 6: How many items were made by their sellers?
+// Answer:
+function question6 () {
+  for (let i = 0; i < data.length; i++){
+
+    composer = data[i].who_made;
+    if (composer === "i_did"){
+sellerItems = composer.length;
+      console.log(sellerItems);}
+  }
 }
